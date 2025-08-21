a98d2b9a8782dabe465ac77d175a5f28b1111b56
sadasda974-create <sadasda974@gmail.com>
2025-08-20 23:18:31 -0700

    Add files via upload

---
+++ splinter_cell_nextbot/lua/entities/nextbot_splinter_cell/shared.lua
@@ -0,0 +1,12 @@
+ENT.Type = "anim"
+ENT.Base = "base_nextbot"
+ENT.PrintName = "Splinter Cell Operative"
+ENT.Author = "AI Assistant"
+ENT.Category = "NextBots"
+ENT.Spawnable = true
+ENT.AdminSpawnable = true
+
+-- Model and properties
+ENT.Model = "models/player/combine_super_soldier.mdl"
+ENT.Health = 200
+ENT.MaxHealth = 200
